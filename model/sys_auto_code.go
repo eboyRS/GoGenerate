4c1cbee113fc81d356ac43f36d4e328b3670e66a
Yunaw <291580817@qq.com>
2020-05-19 14:12:42 +0800

    init

---
+++ model/sys_auto_code.go
@@ -0,0 +1,27 @@
+package model
+
+// 初始版本自动化代码工具
+type AutoCodeStruct struct {
+	StructName   string  `json:"structName"`
+	PackageName  string  `json:"packageName"`
+	Abbreviation string  `json:"abbreviation"`
+	Fields       []Field `json:"fields"`
+}
+
+type Field struct {
+	FieldName  string `json:"fieldName"`
+	FieldType  string `json:"fieldType"`
+	FieldJson  string `json:"fieldJson"`
+	ColumnName string `json:"columnName"`
+}
+
+type TableField struct {
+	ColumnName string `gorm:"column:COLUMN_NAME"`
+	DataType   string `gorm:"column:DATA_TYPE"`
+}
+
+type GoTpl struct {
+	Package  string `json:"package"`
+	FileName string `json:"fileName"`
+	TplPath  string `json:"tplPath"`
+}
